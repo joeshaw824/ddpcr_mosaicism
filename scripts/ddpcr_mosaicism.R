1a43091b935fcca811ff55f8b1dc28501896ab08
joeshaw824 <57156322+joeshaw824@users.noreply.github.com>
2021-12-17 15:27:57 +0000

    Updated scripts for validation plots.

--- scripts/ddpcr_mosaicism.R
+++ scripts/ddpcr_mosaicism.R
@@ -21,13 +21,17 @@ ddPCR_confirmations <- read_excel("ddPCR_designs_confirmations.xlsx",
                                   sheet = "ddPCR_confirmation_list") %>%
   janitor::clean_names()
 
-mosaicism_targets <- read_excel("ddPCR_designs_confirmations.xlsx",
-                                sheet = "targets")
+mosaicism_targets <- read_csv("ddpcr_mosaicism/resources/mosaicism_targets.csv")
 
-analysis_wells <- read_excel("ddPCR_designs_confirmations.xlsx",
-                             sheet = "sample_analysis_wells") %>%
+analysis_wells <- read_csv(
+  "ddpcr_mosaicism/resources/mosaicism_analysis_wells.csv") %>%
   mutate(worksheet_well_sample = paste(worksheet, well, sample, 
-                                       sep = "_"))
+                                       sep = "_"),
+         identity = factor(identity, levels = c("NTC",
+                                                "normal",
+                                                "patient",
+                                                "positive_control")))
+
 #########################
 # Read in ddPCR data 
 #########################
@@ -55,15 +59,15 @@ for (dataFile in ddpcr_files){
   rm(tmp_dat)
 }
 
-# Get one row per sample well.
-
 mosaic_data_wider <- ddpcr_mosaic_data %>% 
   pivot_wider(id_cols = c(worksheet_well_sample, worksheet, well, sample,
                           assay),
               names_from = target_category,
               values_from = c(droplets, positives, 
-                              # Channel 1 +, channel 2 negative (FAM+, VIC-)
+                              # Channel 1 +, channel 2 - (FAM+, VIC-)
                               ch1_ch2_2,
+                              # Channel 1 +, channel 2 + (FAM+, VIC+)
+                              ch1_ch2,
                               concentration,
                               copies_per20u_l_well,
                               fractional_abundance, 
@@ -76,26 +80,32 @@ mosaic_data_wider <- ddpcr_mosaic_data %>%
   select(-c("reference_fractional_abundance", 
             "reference_poisson_fractional_abundance_max",
             "reference_poisson_fractional_abundance_min",
-            "reference_ch1_ch2_2")) %>%
-  dplyr::rename(fam_positives = "variant_ch1_ch2_2")
+            "reference_ch1_ch2_2",
+            "reference_ch1_ch2")) %>%
+  dplyr::rename(fam_positives = "variant_ch1_ch2_2",
+                double_positives = "variant_ch1_ch2")
 
 #########################
 # Get NGS mosaicism percentages 
 #########################
 
 ngs_results <- ddPCR_confirmations %>%
-  mutate(ngs_percent = pmax(mosaic_miner_vaf, rc_vaf,
-                            na.rm = TRUE)*100) %>%
-  select(sample, mosaic_miner_vaf, rc_vaf, ngs_percent)
+  mutate(ngs = ifelse(is.na(mosaic_miner_vaf), rc_vaf,
+                              mosaic_miner_vaf),
+         ngs_percent = ngs*100,
+         mosaic_miner_percent = mosaic_miner_vaf*100) %>%
+  select(sample, mosaic_miner_vaf, rc_vaf, ngs_percent,
+         mosaic_miner_percent)
+
 
 ngs_vs_ddpcr <- mosaic_data_wider %>%
   filter(sample %in% ngs_results$sample) %>%
   filter(worksheet_well_sample %in% analysis_wells$worksheet_well_sample) %>%
   left_join(ngs_results %>%
-              select(sample, ngs_percent),
+              select(sample, ngs_percent, mosaic_miner_percent),
             by = "sample")
 
-# Plot results
+# Plot all results
 ggplot(ngs_vs_ddpcr, aes(x = ngs_percent,
                          y = variant_fractional_abundance)) +
   geom_errorbar(aes(ymin = variant_poisson_fractional_abundance_min,
@@ -108,86 +118,61 @@ ggplot(ngs_vs_ddpcr, aes(x = ngs_percent,
                      breaks = seq(from = 0, to = 11, by = 1)) +
   geom_abline(linetype = "dashed") +
   theme_bw() +
-  labs(x = "ddPCR (%)", y = "NGS (%)",
-       title = "ddPCR vs NGS mosaicism results")
+  theme(panel.grid = element_blank()) +
+  labs(x = "NGS read counter (%)", y = "ddPCR (%)",
+       title = "ddPCR vs NGS read counter results")
 
 #########################
-# Positive droplets in normal controls 
+# FAM positive droplets detected 
 #########################
 
-normal_controls <- c("C236", "20RG-209G0042", "21RG-074G0164",
-                     "20RG-337G0031", "20RG-153G0082")
-
+# Number of single positive droplets in samples
 mosaic_data_wider %>%
-  # Get single well data
-  filter(substr(well, 1, 1) != "M" &
-           sample %in% normal_controls) %>%
-  mutate(sample_type = "normal_control") %>%
-  rbind(
-    mosaic_data_wider %>%
-      filter(worksheet_well_sample %in% analysis_wells$worksheet_well_sample) %>%
-      mutate(sample_type = "patient_sample")) %>%
-  ggplot(aes(x = reorder(worksheet_well_sample, variant_positives),
-             y = fam_positives, 
-             colour = sample_type)) +
-  geom_point() +
+      filter(worksheet_well_sample %in% 
+               analysis_wells$worksheet_well_sample) %>%
+  left_join(analysis_wells %>%
+              select(worksheet_well_sample, identity),
+            by = "worksheet_well_sample") %>%
+  arrange(identity, fam_positives) %>%
+  mutate(worksheet_well_sample = factor(worksheet_well_sample,
+                                        levels = c(worksheet_well_sample))) %>%
+  ggplot(aes(x = worksheet_well_sample,
+             y = fam_positives,
+             colour = identity)) +
+  geom_point(size = 2) +
   theme_bw() +
   theme(axis.text.x = element_blank(),
         axis.ticks.x = element_blank(),
         legend.position = "bottom",
         panel.grid = element_blank(),
         legend.title = element_blank()) +
-  scale_y_continuous(limits = c(0, 300),
-                     breaks = c(0, 10, 50, 100, 200, 300)) +
+  labs(x = "",
+       y = "FAM+VIC- droplets",
+       title = "FAM positive only droplets in samples") +
+  ylim(-10, 400)
+
+# Number of double positive droplets in samples
+mosaic_data_wider %>%
+  filter(worksheet_well_sample %in% 
+           analysis_wells$worksheet_well_sample) %>%
+  left_join(analysis_wells %>%
+              select(worksheet_well_sample, identity),
+            by = "worksheet_well_sample") %>%
+  arrange(identity, variant_positives) %>%
+  mutate(worksheet_well_sample = factor(worksheet_well_sample,
+                                        levels = c(worksheet_well_sample))) %>%
+  ggplot(aes(x = worksheet_well_sample,
+             y = variant_positives,
+             colour = identity)) +
+  geom_point(size = 2) +
+  theme_bw() +
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        legend.position = "bottom",
+        panel.grid = element_blank(),
+        legend.title = element_blank()) +
   labs(x = "",
        y = "FAM+ droplets",
-       title = "FAM positive droplets in samples")
-  #geom_hline(yintercept = 10, linetype = "dashed") +
-  #geom_hline(yintercept = 50, linetype = "dashed")
-
-mosaic_data_wider %>%
-  # Get single well data
-  filter(substr(well, 1, 1) != "M" &
-           sample %in% normal_controls) %>%
-  ggplot(aes(y = variant_positives)) +
-  geom_boxplot() +
-  ylim(0, 20) +
-  labs(x = "",
-       y = "Positive droplets (variant)",
-       title = "Spread of results for normal controls")
-
-#########################
-# Concentration of normal controls 
-#########################
-
-# I was aiming for 25ng input to each 22ul well. Only 20ul is converted
-# into droplets. Assuming a concentration of 1.14ng/ul, that's 22.7ng
-# being measured by the reader.
-# Predicted number of copies detected:
-predicted_copies <- (22.7*1000)/3.3
-
-mosaic_data_wider %>%
-  filter(substr(well, 1, 1) != "M" &
-           # Exclude worksheets which did not have 25ng input
-           !worksheet %in% c("21-0374", "21-2298") &
-           sample == "20RG-209G0042" &
-           reference_concentration != "No Call") %>%
-  ggplot(aes(x = reorder(worksheet_well_sample, reference_copies_per20u_l_well),
-              y = reference_copies_per20u_l_well)) +
-  geom_point(size = 2, pch =21) +
-  theme_bw() +
-  theme(axis.text.x = element_blank()) +
-  ylim(0, 15000) +
-  geom_hline(yintercept = predicted_copies,
-             linetype = "dashed")
-
-test <- mosaic_data_wider %>%
-  filter(substr(well, 1, 1) != "M" &
-           # Exclude worksheets which did not have 25ng input
-           !worksheet %in% c("21-0374", "21-2298") &
-           sample == "20RG-209G0042" &
-           reference_concentration != "No Call") %>%
-  select(worksheet, well, sample, assay,
-         reference_concentration)
+       title = "Positive droplets in samples")
 
 #########################
